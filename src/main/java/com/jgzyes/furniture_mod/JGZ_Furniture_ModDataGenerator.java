dc2486dc510df4118d649449a43002e8a0bfb0ad
JGZSunShineMod1.0.3 <c383260290@gmail.com>
2025-10-25 12:50:37 +0800

    Initial commit

---
+++ src/main/java/com/jgzyes/furniture_mod/JGZ_Furniture_ModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jgzyes.furniture_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JGZ_Furniture_ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
